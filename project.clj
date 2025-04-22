7699e5a9ca86f750b214e06e5ddcaa54a1f8f985
Tim Hawes <tim.hawes@genesys.com>
2025-04-22 11:21:23 -0400

    Update dependencies for ring-mock

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject ring/ring-mock "0.4.0"
+(defproject ring/ring-mock "0.5.0"
   :description "A library for creating mock Ring request maps"
   :url "https://github.com/ring-clojure/ring-mock"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cheshire "5.8.1"]
-                 [ring/ring-codec "1.1.1"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [cheshire "6.0.0"]
+                 [ring/ring-codec "1.3.0"]]
   :plugins [[lein-codox "0.10.3"]]
   :codox
   {:project     {:name "Ring-Mock"}
@@ -13,8 +13,8 @@
    :source-uri  "https://github.com/ring-clojure/ring-mock/blob/{version}/{filepath}#L{line}"}
   :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10" "test"]}
   :profiles
-  {:1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]
+  {:1.8  {:dependencies [[org.clojure/clojure "1.12.0"]]}
+   :1.9  {:dependencies [[org.clojure/clojure "1.12.0"]
                          [ring/ring-spec "0.0.4"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]
+   :1.10 {:dependencies [[org.clojure/clojure "1.12.0"]
                          [ring/ring-spec "0.0.4"]]}})
