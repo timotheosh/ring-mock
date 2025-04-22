11b7546380ef989845191964977b80487ceaf9b8
Tim Hawes <tim.hawes@genesys.com>
2025-04-22 13:42:28 -0400

    Version for me

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring/ring-mock "0.5.0"
+(defproject org.clojars.timotheosh/ring-mock "0.5.0"
   :description "A library for creating mock Ring request maps"
   :url "https://github.com/ring-clojure/ring-mock"
   :license {:name "The MIT License"
